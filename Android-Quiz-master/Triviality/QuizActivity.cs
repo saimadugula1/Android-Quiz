abadb304721365617291a3fba365f4c775d6851b
kishors <saikishore.madugula@gmail.com>
2016-08-25 15:26:27 -0400

    first change

---
+++ Android-Quiz-master/Triviality/QuizActivity.cs
@@ -0,0 +1,129 @@
+﻿using Android.OS;
+using Android.Widget;
+using Android.App;
+using System.Collections.Generic;
+using Android;
+using Android.Views;
+using System;
+using Android.Content;
+
+namespace Triviality
+{
+	[Activity (Label = "Python Quiz", MainLauncher = true, Icon = "@drawable/icon")]
+	public class QuizActivity : Activity
+	{
+		List<Question> quesList;
+		public int score;
+		public int qid=0;
+        string answer;
+		Question currentQ;
+		TextView txtQuestion;
+		RadioButton rda, rdb, rdc;
+		Button butNext;
+
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.QuizLayout);
+			DbHelper db=new DbHelper(this);
+			quesList=db.getAllQuestions();
+			currentQ=quesList[qid];
+			txtQuestion=(TextView)FindViewById(Resource.Id.textView1);
+			rda=(RadioButton)FindViewById(Resource.Id.radio0);
+			rdb=(RadioButton)FindViewById(Resource.Id.radio1);
+			rdc=(RadioButton)FindViewById(Resource.Id.radio2);
+			butNext=(Button)FindViewById(Resource.Id.button1);
+            Button btnSubmit = FindViewById<Button>(Resource.Id.btnSubmit);
+            btnSubmit.Click += btnSubmit_Click;
+            butNext.Click += butNext_Click;
+            setQuestionView();
+		}
+
+         void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var ResultActivity = new Intent(this, typeof(ResultActivity));
+            ResultActivity.PutExtra("Score", score);
+            StartActivity(ResultActivity);
+
+
+          //  StartActivity(typeof(ResultActivity));
+        }
+
+        void  butNext_Click(object sender, EventArgs e)
+        {
+            DbHelper db = new DbHelper(this);
+            quesList = db.getAllQuestions();
+            int Count = quesList.Count;
+            currentQ = quesList[qid];
+       
+         
+                txtQuestion.Text = currentQ.getQUESTION();
+            rda.Text = currentQ.getOPTA();
+            rdb.Text = currentQ.getOPTB();
+            rdc.Text = currentQ.getOPTC();
+            answer = currentQ.getANSWER();
+            if  (rda.Checked==true && rda.Text== answer)
+            {
+                score++;
+            }
+            if (rdb.Checked == true && rdb.Text == answer)
+            {
+                score++;
+            }
+            if (rdc.Checked == true && rdc.Text == answer)
+            {
+                score++;
+              
+            }
+
+            int scr = score;
+           
+            if (qid <( Count-1)) {
+                qid++;
+
+            }
+            else {
+                //var ResultActivity = new Intent(this, typeof(ResultActivity));
+                //ResultActivity.PutExtra("Score", score);
+                //StartActivity(ResultActivity);
+
+
+                //StartActivity(typeof(ResultActivity));
+               
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Congratulations");
+                alert.SetPositiveButton("You are done with quiz, please submit your answers", (senderAlert, args) => {
+                   
+                });
+                
+                RunOnUiThread(() => {
+                    alert.Show();
+                });
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+		{
+			MenuInflater.Inflate(Resource.Menu.QuizMenu, menu);
+			return true;
+		}
+
+		private void setQuestionView()
+		{
+            currentQ = quesList[qid];
+            txtQuestion.Text = currentQ.getQUESTION();
+			rda.Text = currentQ.getOPTA ();
+			rdb.Text = currentQ.getOPTB ();
+			rdc.Text = currentQ.getOPTC ();
+            if (rda.Checked == true && rda.Text == answer)
+            {
+                score++;
+            }
+        }
+	}
+}
+
+
---
+++ Android-Quiz-master/Triviality/ResultActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.OS;
+using Android.App;
+using Android.Widget;
+using Android;
+using Android.Views;
+using Android.Content;
+using System;
+
+namespace Triviality
+{
+    [Activity(Label = "Results")]
+    public class ResultActivity : Activity
+	{
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // int score = Intent.GetIntExtra("Score",-1);
+            SetContentView(Resource.Layout.ResultLayout);
+            //get rating bar object
+            RatingBar bar = (RatingBar)FindViewById(Resource.Id.ratingBar1);
+            bar.NumStars = 5;
+            bar.StepSize = 0.5f;
+            //get text view
+            TextView t = (TextView)FindViewById(Resource.Id.textResult);
+            //get score
+            Bundle b = Intent.Extras;
+            int score = b.GetInt("Score");
+            //QuizActivity k = new QuizActivity();
+            //int score = k.score;
+            //display score
+            bar.Rating = score;
+            if (score <= 2)
+            { t.Text = "Oops, try again bro, keep learning";
+            }
+            if (score > 3 && score <= 4 )
+            { t.Text = "Hmmmm.. maybe you have been reading a lot of Python quiz";
+
+            }
+            if (score >= 5)
+            {
+                t.Text = "Who are you?Are you a geek in python??";
+
+            }
+            //switch (score)
+            //{
+            //         case 0:
+            //case 1:
+            //case 2: t.Text = "Opps, try again bro, keep learning";
+            //	break;
+            //case 3:
+            //case 4:t.Text = "Hmmmm.. maybe you have been reading a lot of JasaProgrammer quiz";
+            //	break;
+            //case 5:t.Text = "Who are you? A student in JP???";
+            //	break;
+            //}
+            Button btnGotoQuiz = FindViewById<Button>(Resource.Id.btnGotoQuiz);
+            btnGotoQuiz.Click += btnGotoQuiz_Click;
+        }
+
+         void btnGotoQuiz_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(QuizActivity));
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+		{
+			MenuInflater.Inflate(Resource.Menu.ResultMenu, menu);
+			return true;
+		}
+	}
+}
+
